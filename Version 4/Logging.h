1630c9d5ab53fa4de5cbe38a6315d27f92a61a9e
Taha Abbasi-Hashemi <taha.ahashemi@gmail.com>
2018-11-11 21:26:59 -0500

    Update Logging.h
    Removed (using namespace std) from ThreadName and therefore had to correct this time as well.

--- Version 4/Logging.h
+++ Version 4/Logging.h
@@ -3,7 +3,6 @@
 #define LOGGING_H
 
 #include "ThreadName.h"
-//#include <chrono>
 #include <ctime> 
 #include <fstream>
 #include <iostream>
@@ -28,17 +27,17 @@ enum ESeverity
 	WARNING,		// Logs showing something may or may not go wrong
 	CRITICAL		// Logs showing fatal death of the program
 };
-string ESeverity2String(ESeverity Serverity);
+std::string ESeverity2String(ESeverity Serverity);
 
 struct Log
 {
-	string Date;
-	string Time;
-	string Thread;
-	string FileName;
+	std::string Date;
+	std::string Time;
+	std::string Thread;
+	std::string FileName;
 	ESeverity Severity;
 	int LineNumber;
-	string Message;
+	std::string Message;
 };
 
 class Logging
@@ -46,42 +45,42 @@ class Logging
 private:
 	bool mAlive;		// This shows the the logger is actively running
 	bool mPaused;		// this shows that the logger is willing to process logs
-	string mDirectory;	// This is the saving location of the logs
-	string mLogFormat;	// This is the order of the variables in the log
-	string mThreadName;	// This is thbe name of the thread processing the logs
-	thread mThread;
-	vector<string> mSemiDirectory;	// This holds the semi direcotires
-	vector<string> mFileNames;		// This holds the file names with in the directories
-	queue<Log> mQueue;	// This is where all unprocesssed logs are stored
+	std::string mDirectory;	// This is the saving location of the logs
+	std::string mLogFormat;	// This is the order of the variables in the log
+	std::string mThreadName;	// This is thbe name of the thread processing the logs
+	std::thread mThread;
+	std::vector<std::string> mSemiDirectory;	// This holds the semi direcotires
+	std::vector<std::string> mFileNames;		// This holds the file names with in the directories
+	std::queue<Log> mQueue;	// This is where all unprocesssed logs are stored
 
 	void WritingFunction(void);		// This is the fucntion being run by the created thread
-	string getFileName(string FileName);
-	string getDirectoryName(string FileName);
-	string getTime(void);
+	std::string getFileName(std::string FileName);
+	std::string getDirectoryName(std::string FileName);
+	std::string getTime(void);
 
 public:
 	Logging();
 	~Logging();
-	void ___mAddLine___(string Message, string FileName, int LineNumber, ESeverity Severity = BASIC);
-#define AddLine(message, severity) ___mAddLine___(message, __FILE__, __LINE__, severity);
-	bool pause(void);
-	bool resume(void);
+	void ___qwerty___(std::string Message, std::string FileName, int LineNumber, ESeverity Severity = BASIC);
+#define AddLine(message, severity) ___qwerty___(message, __FILE__, __LINE__, severity);
+	bool pause(void);	// Pauses the logging process
+	bool resume(void);	// Resumses the logging process
 	bool initalize(void);
 	bool terminate(void);
 	bool getAline(void);
 	bool getPaused(void);
 	bool getEmpty(void);
-	string getDirectory(void);
-	string getLogFormat(void);
-	string getThreadName(void);
-	vector<string> getSemiDirectory(void);
-	bool changeDirectory(string newDirectory);
-	bool changeLogFormat(string newLogFormat);
-	bool changeThreadName(string newThreadName);
-	bool changeSemiDirectory(vector<string> newSemiDirectory);
+	std::string getDirectory(void);
+	std::string getLogFormat(void);
+	std::string getThreadName(void);
+	std::vector<std::string> getSemiDirectory(void);
+	bool changeDirectory(std::string newDirectory);
+	bool changeLogFormat(std::string newLogFormat);
+	bool changeThreadName(std::string newThreadName);
+	bool changeSemiDirectory(std::vector<std::string> newSemiDirectory);
 };
 
-wstring string2wstring(string str);
-string int2string(int num);
+std::wstring string2wstring(std::string & str);
+std::string int2string(int num);
 #endif	// LOGGING_H
 
